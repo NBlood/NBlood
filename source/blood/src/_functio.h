2fbca353b178596bbd9ba133ee1e4a65e6922af0
tmyqlfpir <80724828+tmyqlfpir@users.noreply.github.com>
2025-11-01 14:52:10 +1000

    Blood: Adjust default snapzone values

--- source/blood/src/_functio.h
+++ source/blood/src/_functio.h
@@ -358,8 +358,8 @@ static const int32_t joystickanalogdeaddefaults[MAXJOYAXES] =
 
 static const int32_t joystickanalogsnapzone[MAXJOYAXES] =
    {
-   fix16_from_float(0.15f),
-   fix16_from_float(0.15f),
+   fix16_from_float(0.1f),
+   fix16_from_float(0.1f),
    fix16_from_float(0.4f),
    fix16_from_float(0.1f),
    };
